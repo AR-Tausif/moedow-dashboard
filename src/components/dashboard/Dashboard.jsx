a9d037c32a3905bedd0cf37126e3b961badb86ef
AR-Tausif <tausif.ritu1@gmail.com>
2023-05-07 01:03:57 +0600

    added overflow for scrolling

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,121 +1,20 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar'
 import Profile from './profile/Profile';
-import { FaRegBell, FaUser, FaUserCircle } from "react-icons/fa";
+import Notification from '../../pages/home/notificatioin/Notification';
 
 const Dashboard = () => {
     return (
         <div className='col-span-7 bg-gray-100 h-screen overflow-y-auto'>
             <div className="grid grid-cols-8">
-                <div className="col-span-6">
+                <div className="col-span-6 overflow-y-auto">
                     <Navbar />
+                    <Profile />
+                    <Profile />
                     <Profile />
                 </div>
                 <div className="col-span-2">
-                    <div className="flex justify-between items-center mt-4 bg-slate-200 rounded-lg p-4">
-                        <div className="flex gap-5">
-                            <FaUserCircle className='text-5xl' />
-                            <div className="">
-                                <h3 className="font-bold">Ebad Shafiq</h3>
-                                <p className=" font-medium">UI/UX Designer</p>
-                            </div>
-                        </div>
-                        <FaRegBell className='text-4xl' />
-                    </div>
-
-                    <div className="shadow-sm bg-white p-5 mt-10 rounded-lg">
-                        <div className="flex justify-between items-centers">
-                            <div className="">
-                                <h3 className='text-3xl font-semibold mb-1'>Stocks</h3>
-                                <p>Recent expensive</p>
-                            </div>
-                            <div className="w-10">
-                                <img
-                                    className='rounded-full'
-                                    src="https://user-images.githubusercontent.com/120933222/236634219-05bcd29c-20a3-42cb-9b02-53abe537400b.png" alt="" />
-                            </div>
-                        </div>
-                        <ul className='mt-5'>
-                            <li className='flex justify-between  border-b-4 mt-4'>
-                                <h3 className='font-medium text-lg'>Car Expense</h3>
-                                <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                <h3 className='text-2xl font-extrabold'>...</h3>
-                            </li>
-                            <li className='flex justify-between  border-b-4 mt-4'>
-                                <h3 className='font-medium text-lg'>Car Expense</h3>
-                                <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                <h3 className='text-2xl font-extrabold'>...</h3>
-                            </li>
-                            <li className='flex justify-between  border-b-4 mt-4'>
-                                <h3 className='font-medium text-lg'>Car Expense</h3>
-                                <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                <h3 className='text-2xl font-extrabold'>...</h3>
-                            </li>
-                            <li className='flex justify-between  border-b-4 mt-4'>
-                                <h3 className='font-medium text-lg'>Car Expense</h3>
-                                <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                <h3 className='text-2xl font-extrabold'>...</h3>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="shadow-sm bg-white p-5 mt-10 rounded-lg">
-                        <h3 className='text-3xl font-semibold mb-1'>Clearance</h3>
-                        <ul className='mt-8'>
-                            <li className='flex justify-between pb-3 border-b-4'>
-                                <div className="flex items-center gap-8">
-                                    <div className="bg-gray-200 p-4"><FaUser className='text-2xl' /></div>
-                                    <div className="">
-                                        <h3 className='font-medium'>Car Expense</h3>
-                                        <p>Cycle 3 Wheeler</p>
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                    <p>12 Mar 2023</p>
-                                </div>
-                            </li>
-                            <li className='flex justify-between pb-3 border-b-4 pt-6'>
-                                <div className="flex items-center gap-8">
-                                    <div className="bg-gray-200 p-4"><FaUser className='text-2xl' /></div>
-                                    <div className="">
-                                        <h3 className='font-medium'>Car Expense</h3>
-                                        <p>Cycle 3 Wheeler</p>
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                    <p>12 Mar 2023</p>
-                                </div>
-                            </li>
-                            <li className='flex justify-between pb-3 border-b-4 pt-6'>
-                                <div className="flex items-center gap-8">
-                                    <div className="bg-gray-200 p-4"><FaUser className='text-2xl' /></div>
-                                    <div className="">
-                                        <h3 className='font-medium'>Car Expense</h3>
-                                        <p>Cycle 3 Wheeler</p>
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                    <p>12 Mar 2023</p>
-                                </div>
-                            </li>
-                            <li className='flex justify-between pb-3 border-b-4 pt-6'>
-                                <div className="flex items-center gap-8">
-                                    <div className="bg-gray-200 p-4"><FaUser className='text-2xl' /></div>
-                                    <div className="">
-                                        <h3 className='font-medium'>Car Expense</h3>
-                                        <p>Cycle 3 Wheeler</p>
-                                    </div>
-                                </div>
-                                <div className="">
-                                    <h3 className='font-medium text-red-500'>-366.00 $</h3>
-                                    <p>12 Mar 2023</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+                    <Notification />
                 </div>
             </div>
         </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,39 +1,73 @@
-
-import { FaImage } from 'react-icons/fa';
+import { FaImage } from 'react-icons/fa'
 
 const Sidebar = () => {
-    return (
-        <div className='bg-black h-screen'>
-            <div className="h-4/12">
-                <img src="https://i.ibb.co/nR93F4t/Group-10021.png" alt="" />
-                <span className='text-5xl'>LOGO HERE</span></div>
-            <div className="flex flex-col items-center h-full text-white">
-                <div className="">
-                    <ul>
-                        <li className='border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3'>
-                            <div className="flex justify-center items-center gap-3">
-                                <FaImage className='text-2xl text-red-500' />
-                                <h3>Home</h3>
-                            </div>
-                        </li>
-                        <li className='border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3'>
-                            <div className="flex justify-center items-center gap-3">
-                                <FaImage className='text-2xl text-red-500' />
-                                <h3>Home</h3>
-                            </div>
-                        </li>
-                        <li className='border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3'>
-                            <div className="flex justify-center items-center gap-3">
-                                <FaImage className='text-2xl text-red-500' />
-                                <h3>Home</h3>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div className="justify-end bg-red-700">log out</div>
-            </div>
-        </div>
-    );
-};
 
-export default Sidebar;
+    return (
+
+        <div className="bg-black h-screen flex flex-col">
+
+            <div className="h-4/12">
+
+                <img src="https://i.ibb.co/nR93F4t/Group-10021.png" alt="" />
+
+                <span className="text-5xl">LOGO HERE</span>
+
+            </div>
+
+            <div className="flex flex-col items-center text-white flex-1">
+
+                <div className="">
+
+                    <ul>
+
+                        <li className="border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3">
+
+                            <div className="flex justify-center items-center gap-3">
+
+                                <FaImage className="text-2xl text-red-500" />
+
+                                <h3>Home</h3>
+
+                            </div>
+
+                        </li>
+
+                        <li className="border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3">
+
+                            <div className="flex justify-center items-center gap-3">
+
+                                <FaImage className="text-2xl text-red-500" />
+
+                                <h3>Home</h3>
+
+                            </div>
+
+                        </li>
+
+                        <li className="border-2 border-gray-100 hover:bg-purple-500 w-52 hover:text-white py-3 rounded-lg text-center font-bold mt-3">
+
+                            <div className="flex justify-center items-center gap-3">
+
+                                <FaImage className="text-2xl text-red-500" />
+
+                                <h3>Home</h3>
+
+                            </div>
+
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <div className="justify-end bg-red-700 mt-auto">log out</div>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default Sidebar
