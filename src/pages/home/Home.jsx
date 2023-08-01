2b4ede9a3e6fdfc4a2e35856a4cbcc3d9bfbe67b
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2023-08-01 23:33:36 +0600

    Update Home.jsx
    added console for check error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+
+ React from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Dashboard from '../../components/dashboard/Dashboard';
 
 const Home = () => {
+console.log('hello world')
     return (
         <div className='grid grid-cols-8 gap-2'>
             <Sidebar/>
